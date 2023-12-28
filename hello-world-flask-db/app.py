a16c6dc923ef816cfbca46f690a24f2e395e0743
David Kypuros <davidkypuros@gmail.com>
2023-12-28 10:18:18 -0600

    1

--- hello-world-flask-db/app.py
+++ hello-world-flask-db/app.py
@@ -3,30 +3,34 @@ import psycopg2
 
 app = Flask(__name__)
 
-# Configure the database connection here
+# Database configuration
 db_config = {
     'user': 'developer',
     'password': 'developer',
-    'host': 'postgresql',  # This is the service name in OpenShift
+    'host': 'postgresql',  # Use the service name as the host
     'port': '5432',
     'database': 'sampledb'
 }
 
 @app.route("/")
 def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/create_table")
+def create_table():
     # Connect to the PostgreSQL database
     conn = psycopg2.connect(**db_config)
     cur = conn.cursor()
-    
-    # Perform database operations
-    cur.execute('SELECT version();')
-    db_version = cur.fetchone()
-    
-    # Close connection
+
+    # Create a table called 'test_table' with a single column 'id'
+    cur.execute('CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY);')
+    conn.commit()  # Commit changes to the database
+
+    # Close the connection
     cur.close()
     conn.close()
-    
-    return f"<p>Hello, World! Here is the PostgreSQL version: {db_version[0]}</p>"
+
+    return "<p>Table 'test_table' created successfully!</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
