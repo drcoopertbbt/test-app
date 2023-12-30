1866a597cd92061468685d314fbb2eb64e94b56b
David Kypuros <davidkypuros@gmail.com>
2023-12-29 20:49:20 -0600

    1

--- hello-world-flask-db/entrypoint.sh
+++ hello-world-flask-db/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # entrypoint.sh
 
+# Print all environment variables for debugging
+echo "Printing all environment variables..."
+printenv
+
 # Wait for the Postgres database server to be ready.
 function wait_for_db() {
   local db_host="$1"
   local db_port="$2"
+  echo "Using database host: $db_host"
+  echo "Using database port: $db_port"
   echo "Waiting for database at $db_host:$db_port to be ready..."
   while ! pg_isready -h "$db_host" -p "$db_port" -q; do
     echo "Database not ready. Waiting..."
@@ -20,6 +26,10 @@ function initialize_db() {
   local db_password="$3"
   local db_name="${DB_NAME:-todo_list}"
 
+  echo "Initializing database '$db_name' with user '$db_user'"
+  echo "Using password: $db_password (Note: actual password not printed for security reasons)"
+  echo "Using host: $db_host"
+
   export PGPASSWORD="$db_password"
   echo "Creating database '$db_name' if it doesn't exist..."
   psql -h "$db_host" -U "$db_user" -d "postgres" -tc "SELECT 1 FROM pg_database WHERE datname = '$db_name'" | grep -q 1 || psql -h "$db_host" -U "$db_user" -d "postgres" -c "CREATE DATABASE \"$db_name\""
@@ -34,6 +44,9 @@ function create_schema() {
   local db_user="$2"
   local db_password="$3"
   local db_name="${DB_NAME:-todo_list}"
+
+  echo "Creating schema in database '$db_name' with user '$db_user'"
+  echo "Using host: $db_host"
 
   export PGPASSWORD="$db_password"
 
@@ -52,6 +65,12 @@ EOSQL
 # The host and port of the database server are provided from environment variables.
 DB_HOST="${DB_HOST:-postgresql}"
 DB_PORT="${DB_PORT:-5432}"
+
+echo "Using final DB_HOST: $DB_HOST"
+echo "Using final DB_PORT: $DB_PORT"
+echo "Using final DB_USER: $DB_USER"
+echo "Using final DB_PASSWORD: $DB_PASSWORD (Note: actual password not printed for security reasons)"
+echo "Using final DB_NAME: $DB_NAME"
 
 # Run the wait_for_db, initialize_db, and create_schema functions.
 wait_for_db "$DB_HOST" "$DB_PORT"
