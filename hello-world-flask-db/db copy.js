bdd0fe153300b693b49e7cecea0833702a05198e
David Kypuros <davidkypuros@gmail.com>
2023-12-30 06:17:21 -0600

    1

--- hello-world-flask-db/db copy.js
+++ hello-world-flask-db/db copy.js
@@ -1,5 +1,6 @@
 const { Client } = require("pg");
 
+
 const host = process.env.DB_HOST || "postgresql";
 const user = process.env.DB_USER || "developer";
 const password = process.env.DB_PASSWORD || "test";
@@ -7,11 +8,7 @@ const database = "todo_list";
 const client = new Client({ host, user, database, password });
 
 module.exports = {
-    connect,
-    close,
-    list,
-    create,
-    get,  // Include the get function in the export
+    connect, close, list, create
 }
 
 async function connect() {
@@ -25,10 +22,11 @@ async function connect() {
 }
 
 async function close() {
-    await client.end(); // Ensure proper closure of the client
+    client.close();
 }
 
-async function list() {
+
+async function list(task) {
     const result = await client.query("SELECT * FROM todos");
     return result.rows;
 }
@@ -37,16 +35,12 @@ async function create(task) {
     await client.query("INSERT INTO todos (task) VALUES ($1)", [task]);
 }
 
-async function get(id) {
-    const result = await client.query("SELECT * FROM todos WHERE id = $1", [id]);
-    return result.rows[0]; // Return the first row (the todo item) or undefined if not found
-}
 
-function createSchema() {
+function createSchema(client) {
     return client.query(`
         CREATE TABLE IF NOT EXISTS todos (
             id serial PRIMARY KEY,
             task TEXT NOT NULL
         );
-    `);
+    `)
 }
